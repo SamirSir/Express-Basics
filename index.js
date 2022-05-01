834a3ce421290968d088e39444450575403d250f
SamirSir <acharyasamir421@gmail.com>
2022-05-01 23:33:05 +0545

    post request with error handling

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
-const courses = ['Angular', 'Node', 'Express'];
+const courses = [
+    { id: 1, name: 'Angular' },
+    { id: 2, name: 'Node' },
+    { id: 3, name: 'Express' },
+    { id: 4, name: 'MongoDB' }
+];
 
 app.get('/', (req, res) => {
     res.send('Hello Worlds !!!');
@@ -13,11 +19,28 @@ app.get('/courses', (req, res) => {
     res.send({ courses });
 });
 
+app.post('/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send({
+        id: courses.length + 1,
+        name: req.body.name
+    });
+});
+
 /**
  * reading params
  */
 app.get('/courses/:id', (req, res) => {
-    res.send(courses[req.params.id]);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (course) {
+        res.send(courses[req.params.id]);
+    } else {
+        res.status(404).send({ status: 'error', message: `There is no course with id: ${req.params.id}` })
+    }
 });
 
 /**
