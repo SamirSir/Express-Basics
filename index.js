afb7c9b42a435f9dfded244a0235eedb37a579d4
SamirSir <acharyasamir421@gmail.com>
2022-05-01 22:35:35 +0545

    set environment variabvles use port from process > env object

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Worlds');
+    res.send('Hello Worlds !!!');
 });
 
-app.listen(3000, () => { 'Listening on port 3000 ...' });
+app.get('/courses', (req, res) => {
+    res.send({ courses: ['Angular', 'Node', 'Express'] });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
