329d62df90f9d1dda1aae2953d5dd90122af498a
SamirSir <acharyasamir421@gmail.com>
2022-05-01 22:51:59 +0545

    using params and query from request

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+const courses = ['Angular', 'Node', 'Express'];
 
 app.get('/', (req, res) => {
     res.send('Hello Worlds !!!');
 });
 
 app.get('/courses', (req, res) => {
-    res.send({ courses: ['Angular', 'Node', 'Express'] });
+    res.send({ courses });
 });
 
-const port = process.env.PORT || 3000;
+/**
+ * reading params
+ */
+app.get('/courses/:id', (req, res) => {
+    res.send(courses[req.params.id]);
+});
+
+/**
+ * reading query or optional query-params
+ */
+app.get('/courses/:year/:month', (req, res) => {
+    res.send(req.query)
+});
 
 app.listen(port, () => console.log(`Listening on port ${port} ...`));
